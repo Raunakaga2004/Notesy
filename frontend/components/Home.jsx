d00ef57ca7b6412c5f1a27ac129ef197b6da024f
Raunakaga2004 <raunakaga12@gmail.com>
2025-04-15 09:47:00 +0530

    added note add/edit window

--- frontend/components/Home.jsx
+++ frontend/components/Home.jsx
@@ -8,6 +8,11 @@ import { btn } from "../styles_ui/btn"
 export default function Home(){
     const [user, setUser] = useState({}) //user details except password will be stored
 
+    const [noteWindow, setNoteWindow] = useState(false); // note window to edit or add note // a form to submit
+
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
+
     useEffect(()=>{
         async function fetch(){
             await axios.get("http://localhost:5000/api/auth/profile",{withCredentials : true}) //"withCredentials" allows us to send or receive cookies
@@ -36,6 +41,21 @@ export default function Home(){
             })
     }
     
+    async function addNote(e){
+        e.preventDefault();
+
+        await axios.post("http://localhost:5000/api/main/create", {
+            title : title,
+            content : content
+        }, {withCredentials : true})
+            .then(()=>{
+                setNoteWindow(false);
+                notifyDefault("Note Added!");
+            }).catch((e)=>{
+                console.log(e);
+            })
+    }
+
     return <div className="flex flex-col justify-between h-screen w-screeen">
         {/* Header */}
         <div className="flex flex-row justify-between items-center bg-[#6c757d] h-20 pl-4 pr-4">
@@ -49,12 +69,20 @@ export default function Home(){
             </div>
             }
         </div>
-
-        {/* Content */}
-        <div className="flex flex-col justify-center items-center h-full">
-            <div className="flex flex-row gap-2">Hi <div className="font-semibold italic">{Object.keys(user).length == 0 /*If object is Empty*/ ? "Developer" : user.firstname}!</div></div>
-            <div>Welcome to the DevPage</div>
+        
+        <div>
+            {/* to add note =>  open the note window*/}
+            <button onClick={()=>setNoteWindow(true)}>+</button>
         </div>
+        {noteWindow ? <div>
+                <form onSubmit={(e) => addNote(e)}>
+                    <input type="text" placeholder="Title" onChange={(e)=>setTitle(e.target.value)}></input>
+                    <input type="text" placeholder="Content" onChange={(e)=>setContent(e.target.value)}></input>
+                    <button type="submit">Add Note</button>
+                </form>
+            </div> : <></>
+        }
+
         
     </div>
 }
